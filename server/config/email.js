f606e83fa4a5f66e9242bb283c930f186e91708e
George Psyrras <gpsyrras11@gmail.com>
2025-04-28 00:35:06 +0100

    fix email, add proxy localhost:3000 in pckg.json (dev)

--- server/config/email.js
+++ server/config/email.js
@@ -2,10 +2,14 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  host: 'smtp.gmail.com',
+  // secure: true,
+  // port: 465,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
+
 });
 
 // Function to send an email

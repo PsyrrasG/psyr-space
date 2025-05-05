52115bcf5556a3cc4916245f63200821db27bf05
George Psyrras <gpsyrras11@gmail.com>
2025-05-05 01:07:56 +0100

    add secure and port:465 to transporter conf

--- server/config/email.js
+++ server/config/email.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   host: 'smtp.gmail.com',
-  // secure: true,
-  // port: 465,
+  secure: true,
+  port: 465,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
